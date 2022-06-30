0b7cf57747306ef34b1bb3d9791841c49d3ae2df
gary camboulives <gary.camboulives@gmail.com>
2022-06-30 13:34:59 +0200

    basic tests with mocha ok

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'duck wrong ring column icon ball alone blur hill switch village year',
+  // remember to change this to your own phrase!
+  'https://mainnet.infura.io/v3/32cf1d990f614863a2ba843a12455373'
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
